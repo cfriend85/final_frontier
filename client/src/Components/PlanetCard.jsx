a59aa8d36b9765c08b5669e3fcbce11e66a01f94
Charles <c.friend85@yahoo.com>
2021-11-07 20:34:30 -0700

    Adjusted PlanetCard & img size and Uranus pic

--- client/src/Components/PlanetCard.jsx
+++ client/src/Components/PlanetCard.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import Mars from '../Images/Mars.jpg'
+import Uranus from '../Images/Uranus.jpg'
 const PlanetCard = (props) => {
 
     const [planetPic, setPlanetPic] = useState("");
@@ -12,6 +13,9 @@ const PlanetCard = (props) => {
                 if (props.planet.englishName == "Mars") {
                     setPlanetPic(Mars)
                 }
+                else if(props.planet.englishName == "Uranus"){
+                    setPlanetPic(Uranus)
+                }
                 else{
                     setPlanetPic(res.data.collection.items[6].links[0].href)
                 }
@@ -21,8 +25,8 @@ const PlanetCard = (props) => {
         }, [loaded])
     
     return(
-        <div>
-            <img src={planetPic} alt={props.planet.englishName} />
+        <div className="planetCard">
+            <img src={planetPic} alt={props.planet.englishName} className="planetPic"/>
             <p className="text-info text-decoration-underline">Cool facts about {props.planet.englishName == "Sun"? "The Sun" : props.planet.englishName}</p>
             <p className="text-info">{props.planet.englishName}'s Gravity: {props.planet.gravity}</p>
             <p className="text-info">{props.planet.englishName}'s Average Temperature: {Math.round(props.planet.avgTemp - 273.15) * 9/5 + 32}° F</p>
