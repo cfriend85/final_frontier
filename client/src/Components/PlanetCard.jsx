67e5a9aca6f7aad05b4bd74e1ae6cc5dfa9a7929
Charles <c.friend85@yahoo.com>
2021-11-07 21:10:05 -0700

    Fix planet name for The Sun on PC & update styling

--- client/src/Components/PlanetCard.jsx
+++ client/src/Components/PlanetCard.jsx
@@ -32,9 +32,9 @@ const PlanetCard = (props) => {
         <div className="planetCard">
             <img src={planetPic} alt={props.planet.englishName} className="planetPic"/>
             <h6 className="text-info text-decoration-underline">Cool facts about {props.planet.englishName == "Sun"? "The Sun" : props.planet.englishName}</h6>
-            <p className="text-info">{props.planet.englishName}'s Gravity: {props.planet.gravity}</p>
-            <p className="text-info">{props.planet.englishName}'s Average Temperature: {Math.round(props.planet.avgTemp - 273.15) * 9/5 + 32}° F</p>
-            <p className="text-info">{props.planet.englishName} has {props.planet.moons? props.planet.moons.length : 0} moon(s)!</p>
+            <p className="text-info">{props.planet.englishName == "Sun"? "The Sun" : props.planet.englishName}'s Gravity: {props.planet.gravity}</p>
+            <p className="text-info">{props.planet.englishName == "Sun"? "The Sun" : props.planet.englishName}'s Average Temperature: {Math.round(props.planet.avgTemp - 273.15) * 9/5 + 32}° F</p>
+            <p className="text-info">{props.planet.englishName == "Sun"? "The Sun" : props.planet.englishName} has {props.planet.moons? props.planet.moons.length : 0} moon(s)!</p>
         </div>
     )
 }
