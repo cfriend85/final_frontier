d8a6b8adfa8e5f517c6086c40175ad2575051463
Candersoncoding <soccercda88@aol.com>
2021-11-10 02:27:24 -0600

    Added gif's to PlanetCard

--- client/src/Components/Planet.jsx
+++ client/src/Components/Planet.jsx
@@ -17,6 +17,7 @@ const Planet = (props) => {
             .catch(err => console.log(err))
     };
 
+
     return (
         <div className="pt-3 pb-3 p-4 mx-auto center" onClick={onClickedHandler}>
             <button className="btn btn-outline-secondary text-info btn-lg pnet">{props.item.englishName === "Sun"? "The Sun" : props.item.englishName}</button>
--- client/src/Components/PlanetCard.jsx
+++ client/src/Components/PlanetCard.jsx
@@ -1,38 +1,59 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import { Link } from "@reach/router";
-import Mars from '../Images/Mars.jpg'
-import Uranus from '../Images/Uranus.jpg'
-import Mercury from '../Images/Mercury.jpg'
-import Neptune from '../Images/neptune.jpg'
+import Sun from '../Images/Sun.gif';
+import Mercury from '../Images/Mercury.gif';
+import Venus from '../Images/Venus.gif';
+import Earth from '../Images/Earth.gif';
+import Mars from '../Images/Mars.gif';
+import Jupiter from '../Images/Jupiter.gif'
+import Saturn from '../Images/Saturn.gif'
+import Uranus from '../Images/Uranus.gif'
+import Neptune from '../Images/Neptune.gif'
+import Pluto from '../Images/Pluto.gif'
+
 const PlanetCard = (props) => {
 
     const [planetPic, setPlanetPic] = useState("");
     const [planet, setPlanet] = useState({});
 
     useEffect(() => {
-        axios.get(`https://images-api.nasa.gov/search?q=${props.planet.englishName}`)
-            .then(res => {
-                if (props.planet.englishName === "Mars") {
-                    setPlanetPic(Mars)
-                }
-                else if(props.planet.englishName === "Uranus"){
-                    setPlanetPic(Uranus)
-                }
-                else if(props.planet.englishName === "Mercury"){
-                    setPlanetPic(Mercury)
-                }
-                else if(props.planet.englishName === "Neptune"){
-                    setPlanetPic(Neptune)
-                }
-                else{
-                    setPlanetPic(res.data.collection.items[6].links[0].href)
-                }
-        axios.get(`https://api.le-systeme-solaire.net/rest/bodies/${props.planet.id}`)
-            .then(res => setPlanet(res.data))
-            })
-            .catch(err => console.log(err))
-        }, [props.planet.englishName])
+            axios.get(`https://api.le-systeme-solaire.net/rest/bodies/${props.planet.id}`)
+                .then(res => {
+                    setPlanet(res.data)
+                    if(props.planet.englishName === "Sun"){
+                        setPlanetPic(Sun)
+                    }
+                    else if(props.planet.englishName === "Mercury"){
+                        setPlanetPic(Mercury)
+                    }
+                    else if(props.planet.englishName === "Venus"){
+                        setPlanetPic(Venus)
+                    }
+                    else if(props.planet.englishName === "Earth"){
+                        setPlanetPic(Earth)
+                    }
+                    else if (props.planet.englishName === "Mars") {
+                        setPlanetPic(Mars)
+                    }
+                    else if (props.planet.englishName === "Jupiter") {
+                        setPlanetPic(Jupiter)
+                    }
+                    else if(props.planet.englishName === "Saturn"){
+                        setPlanetPic(Saturn)
+                    }
+                    else if(props.planet.englishName === "Uranus"){
+                        setPlanetPic(Uranus)
+                    }
+                    else if(props.planet.englishName === "Neptune"){
+                        setPlanetPic(Neptune)
+                    }
+                    else{
+                        setPlanetPic(Pluto)
+                    }
+                })
+                .catch(err => console.log(err))
+            }, [props.planet.englishName])
     
     return(
         <div className="planetCard mx-auto center"> 
