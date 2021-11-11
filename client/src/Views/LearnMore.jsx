9e003e2c5a5ef902e8c9aac349487a2aae2550f9
Charles <c.friend85@yahoo.com>
2021-11-10 17:49:37 -0700

    Add NASA planet pic API & onClick to change index

--- client/src/Views/LearnMore.jsx
+++ client/src/Views/LearnMore.jsx
@@ -5,7 +5,8 @@ const LearnMore = (props) => {
 
     const [currentPlanet, setCurrentPlanet] = useState({});
     const [moons, setMoons] = useState([]);
-    const [planetPics, setPlanetPics] = useState([])
+    const [planetPic, setPlanetPic] = useState("")
+    let [planetIndex, setPlanetIndex] = useState(-1)
 
     useEffect(() => {
         axios.get(`https://api.le-systeme-solaire.net/rest/bodies/${props._id}`)
@@ -16,6 +17,14 @@ const LearnMore = (props) => {
             .catch(err => console.log(err))
     }, [props._id])
 
+    const onClickHandler = (event) => {
+        event.preventDefault();
+        setPlanetIndex(planetIndex += 1)
+        axios.get(`https://images-api.nasa.gov/search?q=${currentPlanet.englishName}`)
+            .then(res => setPlanetPic(res.data.collection.items[planetIndex].links[0].href))
+            .catch(err => console.log(err))
+    }
+
     return(
         <div>
             <h1>Learn More page</h1>
@@ -25,6 +34,9 @@ const LearnMore = (props) => {
                     return <p key={i}>{item.moon}</p>
                 })
             }
+
+            <button className="btn btn-warning" onClick={onClickHandler}>Map Images</button>
+            {planetPic? <img src={planetPic} alt={currentPlanet.englishName}/> : <p></p>}
         </div>
     )
 }
